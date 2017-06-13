51e7d920943f9dc8189a55afcc7ef60d463302b3
luinu <krzysztof.sasin1987@gmail.com>
2017-06-13 20:12:50 +0200

    first commit

---
+++ lib/discogs.rb
@@ -0,0 +1,10 @@
+require 'bundler/setup'
+
+require 'json'
+require 'faraday'
+require 'awesome_print'
+
+require 'discogs/json_client'
+require 'discogs/discogs_client'
+require 'discogs/discogs_app'
+require 'discogs/album_formatter'
